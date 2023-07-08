7bb680de88b89ae21dbb0d0110d9a4245f7d0909
Paul Staab <paulstaab@users.noreply.github.com>
2023-07-08 14:20:00 +0200

    Add a simple testing framework (#3)

---
+++ tests/__init__.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from pandas import DataFrame as PandasDF
+from pyspark.sql import DataFrame as SparkDF, SparkSession, functions as sparkfunc
+
+from typing import Callable, Any
+
+from polarspark.sql import (
+    DataFrame as PolarsparkDF,
+    SparkSession as PolarsparkSession,
+    functions as polarsparkfunc,
+)
+
+DataFrame = SparkDF | PolarsparkDF
+
+
+def assert_df_transformation_equal(
+    data: PandasDF, transformation: Callable[[DataFrame, Any], DataFrame]
+) -> None:
+    spark = SparkSession.builder.getOrCreate()
+    spark_df = transformation(spark.createDataFrame(data), sparkfunc).toPandas()
+
+    polarspark = PolarsparkSession.builder.getOrCreate()
+    polarspark_df = transformation(
+        polarspark.createDataFrame(data), polarsparkfunc
+    ).toPandas()
+
+    pd.testing.assert_frame_equal(polarspark_df, spark_df)
