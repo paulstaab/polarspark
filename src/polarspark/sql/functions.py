4e88b65358011236f42a3402b76b866a76dbf96f
Paul Staab <paulstaab@users.noreply.github.com>
2023-07-11 17:33:40 +0000

    Refactor alignment of column names

--- src/polarspark/sql/functions.py
+++ src/polarspark/sql/functions.py
@@ -11,12 +11,21 @@ def column(col: str) -> Column:
     return pl.col(col)
 
 
+def _get_base_col_name(col: Column) -> str | None:
+    """Return the name of the column for a column created with col or column.
+
+    Returns None if the column is a more complex column.
+    """
+    if str(col).startswith('col("'):
+        return str(col)[5:-2]  # Get just the name without the col()
+    return None
+
+
 def sum(col: str | Column) -> Column:
     if isinstance(col, str):
         return column(col).sum().alias(f"sum({col})")
 
-    if str(col).startswith('col("'):
-        col_name = str(col)[5:-2]  # Get just the name without the col()
+    if col_name := _get_base_col_name(col):
         return col.sum().alias(f"sum({col_name})")
 
     return col.sum()
@@ -33,8 +42,7 @@ def max(col: str | Column) -> Column:
     if isinstance(col, str):
         return column(col).max().alias(f"max({col})")
 
-    if str(col).startswith('col("'):
-        col_name = str(col)[5:-2]  # Get just the name without the col()
-        return col.max().alias(f"max({col_name})")
+    if col_name := _get_base_col_name(col):
+        return col.sum().alias(f"max({col_name})")
 
     return col.max()
